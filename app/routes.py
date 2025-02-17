9bb9fb8c7fee415912baf1a0da5039365332402e
Olya Naboychenko <olyanaboychenko@gmail.com>
2025-02-17 16:51:43 +0300

    Registration was added class RegistrationForm, register.html, new route func register

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,12 @@
 from app import app
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import db
 from app.models import User
 from urllib.parse import urlsplit
-import logging
+
 
 @app.route('/')
 @app.route('/index')
@@ -46,5 +46,18 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
